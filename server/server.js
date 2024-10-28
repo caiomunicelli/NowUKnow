52edc3cecdd07e04906c47ebb7aec0da9be2ca1b
Lorenzo Messias Lacerda <lorenzo.messias2@gmail.com>
2024-10-28 18:36:27 -0300

    Reestruturação

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
+const clientPath = path.join(__dirname, '../client/')
 const app = express();
 const port = 3000; // Porta que a aplicação irá escutar
 
 // Rota principal
 app.get('/', (req, res) => {
-  res.send('Bem-vindo ao NowUKnow! Plataforma de aprendizado democrático.');
+  res.sendFile('/index.html', {root : clientPath});
 });
 
 // Inicia o servidor
