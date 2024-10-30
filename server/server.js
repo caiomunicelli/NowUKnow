d233e8e550e12429111a2ead705b4fedca9d271f
Lorenzo Messias Lacerda <lorenzo.messias2@gmail.com>
2024-10-30 20:57:29 -0300

    Retiro do um import não utilizado no server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const UserRepository = require('./controllers/usuarioController.js');
 const path = require('path');
 const clientPath = path.join(__dirname, '../client/')
 
