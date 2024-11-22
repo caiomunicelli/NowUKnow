0f458c20c76fb448ff19dd28b8c94407c9b1f80d
LuizGuiTM <l.guilhermedesouza@gmail.com>
2024-11-21 23:46:43 -0300

    barra de pesquisa

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Select from "react-select";
 import { useAuthContext } from "../contexts/AuthContext";
 import { fetchUsuarioLogado } from "../services/usuarioService";
 import Sidebar from "./Sidebar";
 import { Login } from "./";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Importa os ícones
 import "./Navbar.css";
+
+const options = [
+  {
+    value: "autor",
+    label: (
+      <div className="nowuknow-select-container">
+        <i className="bi bi-person"></i>
+        <span>Autor</span>
+      </div>
+    ),
+    icon: "bi bi-person",
+    default: false,
+  },
+  {
+    value: "conteudo",
+    label: (
+      <div className="nowuknow-select-container">
+        <i className="bi bi-file-earmark-text"></i>
+        <span>Conteúdo</span>
+      </div>
+    ),
+    icon: "bi bi-file-earmark-text",
+    default: true,
+  },
+  {
+    value: "categoria",
+    label: (
+      <div className="nowuknow-select-container">
+        <i className="bi bi-folder2"></i>
+        <span>Categoria</span>
+      </div>
+    ),
+    icon: "bi bi-folder2",
+    default: false,
+  },
+];
+
 
 function Navbar({ isLoginMenuOpen, setIsLoginMenuOpen }) {
   const { isAuthenticated, logout } = useAuthContext();
   const [usuario, setUsuario] = useState(null);
   const [erro, setErro] = useState(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(
+    options.find((opt) => opt.default) || options[0]
+  );
   const location = useLocation();
 
   const toggleSidebar = () => {
@@ -19,6 +61,42 @@ function Navbar({ isLoginMenuOpen, setIsLoginMenuOpen }) {
 
   const toggleLoginMenu = () => {
     setIsLoginMenuOpen(!isLoginMenuOpen);
+  };
+
+  const customStyles = {
+    control: (base) => ({
+      ...base,
+      minWidth: "50px",
+      height: "36px",
+      border: "1px solid #ccc",
+      borderRadius: "25px",
+      boxShadow: "none",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }),
+    option: (base, { isFocused, isSelected }) => ({
+      ...base,
+      display: "flex",
+      alignItems: "center",
+      color: isSelected ? "var(--nowuknow-text-muted)" : "#333",
+      backgroundColor: isFocused ? "#f0f0f0" : "#fff",
+      cursor: "pointer",
+      ":hover": { backgroundColor: "#f8f9fa" },
+    }),
+    singleValue: (base) => ({
+      ...base,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }),
+    dropdownIndicator: (base) => ({
+      ...base,
+      display: "none",
+    }),
+    indicatorSeparator: () => ({
+      display: "none",
+    }),
   };
 
   useEffect(() => {
@@ -41,12 +119,15 @@ function Navbar({ isLoginMenuOpen, setIsLoginMenuOpen }) {
     }
   }, [isAuthenticated]);
 
-  // Fecha o menu de login sempre que a rota mudar
   useEffect(() => {
     if (isLoginMenuOpen) {
       setIsLoginMenuOpen(false);
     }
   }, [location]);
+
+  const handleChange = (selected) => {
+    setSelectedOption(selected);
+  };
 
   return (
     <header className="nowuknow-header">
@@ -64,6 +145,16 @@ function Navbar({ isLoginMenuOpen, setIsLoginMenuOpen }) {
             className="nowuknow-search-form nowuknow-search hide-on-resize-2"
             role="search"
           >
+            <Select
+              options={options.map((option) => ({
+                value: option.value,
+                label: option.label ,
+              }))}
+              value={selectedOption}
+              onChange={handleChange}
+              styles={customStyles}
+              isSearchable={false}
+            />
             <input
               type="search"
               placeholder="Busque algum conteúdo, categoria ou autor"
@@ -108,9 +199,7 @@ function Navbar({ isLoginMenuOpen, setIsLoginMenuOpen }) {
               )}
             </li>
           </ul>
-          {isLoginMenuOpen && (
-            <Login onClose={() => setIsLoginMenuOpen(false)} />
-          )}
+          {isLoginMenuOpen && <Login onClose={() => setIsLoginMenuOpen(false)} />}
         </div>
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       </nav>
